19830758560f9211e7fa624fc05db37a36a28fb3
Alexandr Vasilchenko <sanloid2@gmail.com>
2021-04-24 11:45:05 +0600

    Страшно, очень страшно. Если бы мы знали что это такое, но мы не знаем что это такое

--- web/script.js
+++ web/script.js
@@ -12,3 +12,20 @@ async function rendered() {
     // в петон, из питона ничо получать не надо (пока что вроде как)
     let res = await eel.render_doc(programm_discipline, number_direction, name_direction, decryption)();
 }
+
+
+
+var colors = ['1abc9c', '2c3e50', '2980b9', '7f8c8d', 'f1c40f', 'd35400', '27ae60'];
+
+colors.each(function (color) {
+  $$('.color-picker')[0].insert(
+    '<div class="square" style="background: #' + color + '"></div>'
+  );
+});
+
+$$('.color-picker')[0].on('click', '.square', function(event, square) {
+  background = square.getStyle('background');
+  $$('.custom-dropdown select').each(function (dropdown) {
+    dropdown.setStyle({'background' : background});
+  });
+});
